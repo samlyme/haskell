09618e8270601c2d249b2b9fac583862840eb4bc
Sam Ly <sam@samly.me>
2025-04-05 21:44:41 -0400

    use internal module

--- hello.hs
+++ hello.hs
@@ -4,13 +4,13 @@ main :: IO ()
 main = putStrLn (render myhtml)
 
 myhtml :: Html
-myhtml = 
+myhtml =
   html_
     "My title"
     ( append_
-      (h1_ "Heading")
-      ( append_
-        (p_ "Paragraph #1")
-        (p_ "Paragraph #2")
-      )
+        (h1_ "Heading")
+        ( append_
+            (p_ "Paragraph #1 <<")
+            (p_ "Paragraph #2")
+        )
     )
