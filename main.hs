46286c3ee848f0c8e9db1045db18c99fd5ecf6ab
Sam Ly <sam@samly.me>
2025-05-17 20:43:21 -0700

    hello world

---
+++ main.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -Wmissing-signatures #-}
+
+main :: IO ()
+main = putStrLn (makeHtml "My page title" "My page content")
+
+makeHtml :: String -> String -> String
+makeHtml title content =
+  html_
+    ( head_ (title_ title) <> body_ content
+    )
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
