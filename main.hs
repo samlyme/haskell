40b0039f2dee915e1d6517d405f19c9b67606082
Sam Ly <sam@samly.me>
2025-05-17 20:57:58 -0700

    lambda

--- main.hs
+++ main.hs
@@ -1,7 +1,15 @@
 {-# OPTIONS_GHC -Wmissing-signatures #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Avoid lambda" #-}
 
 main :: IO ()
-main = putStrLn (makeHtml "My page title" "My page content")
+main =
+  putStrLn
+    (makeHtml "My page title" ("Cool number: " <> show 3))
+
+three :: Int
+three = (\a b -> a + b) 2 1
 
 makeHtml :: String -> String -> String
 makeHtml title content =
@@ -20,6 +28,12 @@ head_ = el "head"
 
 title_ :: String -> String
 title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
