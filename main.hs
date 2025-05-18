79b2fa230649582e150839dd2922054b97847cd7
Sam Ly <sam@samly.me>
2025-05-17 21:38:35 -0700

    working page

--- main.hs
+++ main.hs
@@ -4,45 +4,35 @@
 {-# HLINT ignore "Avoid lambda" #-}
 
 main :: IO ()
-main =
-  putStrLn
-    (makeHtml "My page title" ("Cool number: " <> show 3))
+main = putStrLn (render page)
+
+page :: Html
+page =
+  html_
+    "my title"
+    ( append_
+        (h1_ "urmom")
+        ( append_
+            (p_ "p1")
+            (p_ "p2")
+        )
+    )
 
 newtype Html = Html String
 
 newtype Structure = Structure String
 
--- If you squint, (Structure str) is how we create a variable of type Structure
--- Since everything is functional, having this definition in the args
--- gives us access to the underlying data.
-getStructureString :: Structure -> String
-getStructureString (Structure str) = str
+type Title = String
 
-three :: Int
-three = (\a b -> a + b) 2 1
-
-makeHtml :: String -> String -> String
-makeHtml title content =
-  getStructureString
-    ( html_
-        ( getStructureString (append_ (title_ title) (body_ content))
+html_ :: String -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" title)
+            <> el "body" (getStructureString content)
         )
     )
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = Structure (a <> b)
-
-html_ :: String -> Structure
-html_ = Structure . el "html"
-
-body_ :: String -> Structure
-body_ = Structure . el "body"
-
-head_ :: String -> Structure
-head_ = Structure . el "head"
-
-title_ :: String -> Structure
-title_ = Structure . el "title"
 
 p_ :: String -> Structure
 p_ = Structure . el "p"
@@ -52,3 +42,15 @@ h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+-- If you squint, (Structure str) is how we create a variable of type Structure
+-- Since everything is functional, having this definition in the args
+-- gives us access to the underlying data.
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render (Html a) = a
