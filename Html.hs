151bde941afeda2b073dc7fd39d2778b1e1c6451
Sam Ly <sam@samly.me>
2025-05-17 22:00:07 -0700

    extract html

---
+++ Html.hs
@@ -0,0 +1,65 @@
+{-# OPTIONS_GHC -Wmissing-signatures #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use concatMap" #-}
+
+module Html (
+  Html,
+  Title,
+  Structure,
+  html_,
+  p_,
+  h1_,
+  append_,
+  render
+) where
+
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+type Title = String
+
+html_ :: String -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" title)
+            <> el "body" (getStructureString content)
+        )
+    )
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+-- If you squint, (Structure str) is how we create a variable of type Structure
+-- Since everything is functional, having this definition in the args
+-- gives us access to the underlying data.
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+
+escape :: [Char] -> [Char]
+escape =
+  let 
+    escapeChar c = 
+      case c of
+        '<' -> "&lt;"
+        '>' -> "&gt;"
+        '&' -> "&amp;"
+        '"' -> "&quot;"
+        '\'' -> "&#39;"
+        _ -> [c]
+  in
+    concat . map escapeChar
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render (Html a) = a
--- main.hs
+++ main.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS_GHC -Wmissing-signatures #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Avoid lambda" #-}
+import Html ( Html, html_, p_, h1_, append_, render )
 
 main :: IO ()
 main = putStrLn (render page)
@@ -17,40 +16,3 @@ page =
             (p_ "p2")
         )
     )
-
-newtype Html = Html String
-
-newtype Structure = Structure String
-
-type Title = String
-
-html_ :: String -> Structure -> Html
-html_ title content =
-  Html
-    ( el
-        "html"
-        ( el "head" (el "title" title)
-            <> el "body" (getStructureString content)
-        )
-    )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
--- If you squint, (Structure str) is how we create a variable of type Structure
--- Since everything is functional, having this definition in the args
--- gives us access to the underlying data.
-getStructureString :: Structure -> String
-getStructureString (Structure str) = str
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = Structure (a <> b)
-
-render :: Html -> String
-render (Html a) = a
